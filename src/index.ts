a522c2829979d71ee13d4450425a60c3d3bf3ad3
Nailton Magalhães <nailton85@gmail.com>
2023-07-26 20:26:29 -0300

    XmlHttpRequest, Sério? (#6)

--- src/index.ts
+++ src/index.ts
@@ -2,28 +2,33 @@
 import { fromEvent } from "rxjs";
 import { delay, filter, map } from "rxjs/operators";
 
-interface mouseTrack {
-    x: number;
-    y: number;
+interface IMovie {
+    title: string;
 }
 
-let circle = document.getElementById('circle');
-let source = fromEvent(document, 'mousemove').pipe(
-    map((e: MouseEvent) => {
-        return { x: e.clientX, y: e.clientY };
-    }),
-    filter((value: mouseTrack) => value.x < 500),
-    delay(1000)
-);
+let button = document.getElementById('button');
+let output = document.getElementById('output');
 
-function onNext(value: mouseTrack) {
-    console.log(value);
-    circle.style.left = `${value.x}px`;
-    circle.style.top = `${value.y}px`;
+let click = fromEvent(button, 'click');
+
+function load(url: string) {
+    let xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', () => {
+        let movies = JSON.parse(xhr.responseText);
+
+        movies.forEach((movie: IMovie) => {
+            let div = document.createElement('div');
+            div.innerText = movie.title;
+            output.appendChild(div);
+        });
+    })
+
+    xhr.open('GET', url);
+    xhr.send();
 }
 
-source.subscribe({
-    next: (value: mouseTrack) => onNext(value),
+click.subscribe({
+    next: () => load('../movies.json'),
     error: (e: Error) => console.log(e),
     complete: () => console.log()
 })
