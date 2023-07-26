bf61a8dcf53b5d52fb9d8f98b4dd50d9039cff2f
Nailton Magalhaes <nailton85@gmail.com>
2023-07-25 21:29:02 -0300

    Mouse Events - Parte 01

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,27 @@
-import { Observable } from "rxjs";
-import { filter, map } from "rxjs/operators";
 
-let numbers = [1, 5, 10, 15, 20, 25, 30];
+import { fromEvent } from "rxjs";
+import { delay, filter, map } from "rxjs/operators";
 
-let source = new Observable(subscriber => {
-    let index = 0;
-    let produceValue = () => {
-        subscriber.next(numbers[index++]);
-        if (index < numbers.length) {
-            setTimeout(produceValue, 500);
-        } else {
-            subscriber.complete();
-        }
-    }
-    produceValue();
+interface mouseTrack {
+    x: number;
+    y: number;
+}
+
+let circle = document.getElementById('circle');
+let source = fromEvent(document, 'mousemove').pipe(
+    map((e: MouseEvent) => {
+        return { x: e.clientX, y: e.clientY };
+    }),
+    filter((value: mouseTrack) => value.x < 500),
+    delay(3000)
+);
+
+function onNext(value: mouseTrack) {
+    console.log(value);
+}
+
+source.subscribe({
+    next: (value: mouseTrack) => onNext(value),
+    error: (e: Error) => console.log(e),
+    complete: () => console.log()
 })
-
-source.pipe(
-    map((n: number) => n * 2)
-).subscribe({
-    next: (x: number) => {
-        console.log(`Pipe: ${x}`)
-    },
-    error: (e: Error) => console.log(e),
-    complete: () => console.log('Complete'),
-});
-
-source.pipe(
-    filter((n: number) => n > 5)
-).subscribe({
-    next: (x: number) => {
-        console.log(`Filter: ${x}`)
-    },
-    error: (e: Error) => console.log(e),
-    complete: () => console.log('Complete'),
-});
