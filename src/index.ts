01b57ab2505b212cb4340aa0bcb07e5b6e6c9ce4
Nailton Magalhães <nailton85@gmail.com>
2023-07-26 21:24:54 -0300

    Retry, RetryWhen... (scan, takeWhile, delay) (#8)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 
 import { Observable, fromEvent } from "rxjs";
-import { delay, filter, map, switchMap } from "rxjs/operators";
+import { delay, filter, map, switchMap, retry, retryWhen, scan, takeWhile } from "rxjs/operators";
 
 interface IMovie {
     title: string;
@@ -16,14 +16,31 @@ function load(url: string): Observable<any> {
         let xhr = new XMLHttpRequest();
         output.innerHTML = '';
         xhr.addEventListener('load', () => {
-            let data = JSON.parse(xhr.responseText);
-            subscriber.next(data);
-            subscriber.complete();
+            if (xhr.status == 200) {
+                let data = JSON.parse(xhr.responseText);
+                subscriber.next(data);
+                subscriber.complete();
+            } else {
+                subscriber.error(xhr.statusText);
+            }
         })
 
         xhr.open('GET', url);
         xhr.send();
-    })
+    }).pipe(
+        //retryWhen(retryStrategy({ attempt: 4, timeDelay: 500 }))
+        retry({ count: 3, delay: 1000 })
+    )
+}
+
+function retryStrategy({ attempt = 3, timeDelay = 1000 }) {
+    return (errors: Observable<any>) => {
+        return errors.pipe(
+            scan((acc, value) => acc + 1, 0),
+            takeWhile(acc => acc < attempt),
+            delay(timeDelay)
+        )
+    }
 }
 
 function renderMovie(movies: IMovie[]) {
@@ -35,9 +52,9 @@ function renderMovie(movies: IMovie[]) {
 }
 
 click.pipe(
-    switchMap(() => load('../movies.json'))
+    switchMap(() => load('./movies.json'))
 ).subscribe({
     next: renderMovie,
-    error: (e: Error) => console.log(e),
-    complete: () => console.log()
+    error: (e: Error) => console.log(`Erro: ${e}`),
+    complete: () => console.log("Complete")
 })
