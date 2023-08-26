4888aa0db7afacf348453564760bfa702e14f5a3
Nailton Magalhaes <nailton85@gmail.com>
2023-08-26 18:48:08 -0300

    Adding users module

--- ProjectSchool/src/app/app.module.ts
+++ ProjectSchool/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './shared/materials/Material.module';
+import { MaterialModule } from './shared/materials/material.module';
 
 @NgModule({
   declarations: [
