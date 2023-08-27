16add0fd49df4dae5bc346ff0daa6911c0da297b
Nailton Magalhaes <nailton85@gmail.com>
2023-08-27 19:41:02 -0300

    Toolbar dynamic menu created

--- ProjectSchool/src/app/app-routing.module.ts
+++ ProjectSchool/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   {
     path: 'users',
     loadChildren: () => import('./pages/users/users.module').then(m => m.UsersModule)
