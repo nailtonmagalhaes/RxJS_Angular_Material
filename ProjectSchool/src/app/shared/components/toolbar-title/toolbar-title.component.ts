0c98f2c609661de97e9cb992a3b1b746f51263d4
Nailton Magalhaes <nailton85@gmail.com>
2023-08-26 23:01:39 -0300

    Adjusting the title component

--- ProjectSchool/src/app/shared/components/toolbar-title/toolbar-title.component.ts
+++ ProjectSchool/src/app/shared/components/toolbar-title/toolbar-title.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-toolbar-title',
   templateUrl: './toolbar-title.component.html',
   styleUrls: ['./toolbar-title.component.scss']
 })
-export class ToolbarTitleComponent {
+export class ToolbarTitleComponent implements OnInit {
+
+  @Input() title: string = "";
+  @Input() icon: string = "";
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+  }
 
 }
