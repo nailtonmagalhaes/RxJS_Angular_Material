1b49236448d9e19673083abed7a6d5b25122cc1c
Nailton Magalhaes <nailton85@gmail.com>
2023-08-27 15:37:53 -0300

    Dynamic title created

--- ProjectSchool/src/app/app.component.ts
+++ ProjectSchool/src/app/app.component.ts
@@ -1,9 +1,10 @@
 
 import { BreakpointObserver } from '@angular/cdk/layout';
-import { Component } from '@angular/core';
-import { fromEvent, map } from 'rxjs';
+import { Component, inject } from '@angular/core';
+import { filter, fromEvent, map } from 'rxjs';
 import { MenuItem } from './shared/models/menuItem';
 import { menuItems } from './shared/models/menu';
+import { NavigationEnd, Router } from '@angular/router';
 
 export const SCROLL_CONTAINER = 'mat-sidenav-content';
 export const TEXT_LIMIT = 50;
@@ -19,20 +20,33 @@ export class AppComponent {
   public isSmallScreen = false;
   public popText = false;
   public applyShadow = false;
-  public items_menu: MenuItem[]=menuItems;
+  public items_menu: MenuItem[] = menuItems;
+  private breakpointObserver: BreakpointObserver;
+  private route: Router;
+  public menuName = '';
 
-  constructor(private breakpointObserver: BreakpointObserver) { }
+  constructor() {
+    this.breakpointObserver = inject(BreakpointObserver);
+    this.route = inject(Router);
+  }
 
   ngOnInit(): void {
     const content = document.getElementsByClassName(SCROLL_CONTAINER)[0];
 
     fromEvent(content, 'scroll')
-      .pipe(
-        map(() => content.scrollTop)
-      )
-      .subscribe({
-        next: (value: number) => this.determineHeader(value)
-      })
+      .pipe(map(() => content.scrollTop))
+      .subscribe((value: number) => this.determineHeader(value));
+
+    this.route.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(event => event as NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      let moduleName = event.url.split('/')[1];
+
+      console.log(event.url);
+
+      this.menuName = this.items_menu.filter((item: MenuItem) => item.link == `/${moduleName}`)[0].label;
+    });
   }
 
   determineHeader(scrollTop: number) {
